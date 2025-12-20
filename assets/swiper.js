351eb77cfabadaa273666ad6d8919407aba5ce2f
muhammedfayazp <muhammedfayaz@yahoo.in>
2025-12-20 19:09:21 +0400

    revert

--- assets/swiper.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    if (typeof Swiper !== 'undefined') {
-        new Swiper('.swiper', {
-            loop: true,
-            autoplay: { delay: 4000 },
-            pagination: { el: '.swiper-pagination' }
-        });
-    }
-});
